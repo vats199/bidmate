e5bfcef5368214aa4b4e574326738a69e8bde081
Vatsal Patel <vatsalp.tcs@gmail.com>
2023-09-08 18:22:52 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="auctionContainer">
+        <div className="playerDetailsContainer">
+          <img src="https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-02.jpg" />
+          <div className="detailsContainer">
+            <span>Name</span>
+            <span>Age</span>
+            <span>Hand</span>
+            <span>Gender</span>
+            <span>Type</span>
+            <span>Base Price</span>
+          </div>
+        </div>
+        <div>Skills</div>
+        <div className="bidsContainer">
+          <div>Current</div>
+          <div>Older</div>
+        </div>
+      </div>
+      <div className="infoContainer">
+        <div className="walletContainer">Wallet</div>
+        <div className="soldPlayersContainer">Sold Players</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
